2d225337527eaada2d3dbddc7da149d179ace5f3
bbourassa <bbourassa@umass.edu>
2021-12-18 17:43:54 -0500

    email updates

--- index.js
+++ index.js
@@ -1,7 +1,8 @@
 let express = require('express'),
   path = require('path'),
   cors = require('cors'),
-  bodyParser = require('body-parser')
+  bodyParser = require('body-parser'),
+  nodemailer = require('nodemailer')
 
 //const tripRoute = require('./node-rest-api/routes/trip.routes');
 
@@ -18,6 +19,41 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({
   extended: false
 }));
+
+process.env['NODE_TLS_REJECT_UNAUTHORIZED'] = 0;
+
+const sendMail = (user, callback) => {
+    console.log(user);
+    const transporter = nodemailer.createTransport({
+      host: "smtp.gmail.com",
+      port: 587,
+      secure: false,
+      auth: {
+        user: "bnboura3@gmail.com",
+        pass: "thisistemp"
+      }
+    });
+
+    const mailOptions = {
+        from: user.email,
+        to: 'bnboura3@gmail.com',
+        subject: "Gym Inquiry",
+        html: `<h2>You Have a New Inquiry from ${user.name}:</h1><br>
+        <h3>Contact Email: ${user.email}</h3>
+        <p>${user.message}</p>`
+      };
+    
+    const mailReceipt = {
+        from: user.email,
+        to: user.email,
+        subject: "Thank You For Your Inquiry!",
+        html: `<p>Thank you for your inquiry to Omnigym. We will get back to you as soon as possible.</p>` 
+    }
+      
+      transporter.sendMail(mailOptions, callback);
+      transporter.sendMail(mailReceipt, callback);
+  }
+
 app.use(cors());
 
 app.use(allowCrossDomain);
@@ -41,6 +77,22 @@ app.get('/', (req, res) => {
 app.get('*', (req, res) => {
   res.sendFile(path.join(__dirname, 'dist/omni-dev'));
 });
+
+// define a sendmail endpoint, which will send emails and response with the corresponding status
+app.post("/sendmail", (req, res) => {
+    console.log("request came");
+    let user = req.body;
+    sendMail(user, (err, info) => {
+      if (err) {
+        console.log(err);
+        res.status(400);
+        res.send({ error: "Failed to send email" });
+      } else {
+        console.log("Email has been sent");
+        res.send(info);
+      }
+    });
+  });
 
 //app.get('/main-menu', (req, res) => {
 //    res.sendFile(path.resolve(__dirname,'dist/trip-planner/index.html'));
--- src/app/homepage/homepage.component.ts
+++ src/app/homepage/homepage.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { FormBuilder, FormControl, FormGroup, Validators } from '@angular/forms';
 import { HttpClient } from '@angular/common/http';
+import { MessageService } from '../message.service';
 
 @Component({
   selector: 'app-homepage',
@@ -9,62 +10,32 @@ import { HttpClient } from '@angular/common/http';
 })
 export class HomepageComponent implements OnInit {
   
-  form!: FormGroup;
-  fullname: FormControl = new FormControl('', [Validators.required]);
-  email: FormControl = new FormControl('', [Validators.required, Validators.email]);
-  message: FormControl = new FormControl('', [Validators.required, Validators.maxLength(256)]);
-  honeypot: FormControl = new FormControl(''); //used to prevent spam
-  submitted: boolean = false; //show and hide the success message
-  isLoading: boolean = false; //disable the submit button if we're loading
-  responseMessage!: string; //the response message to show to the user
+  FormData!: FormGroup;
 
-  constructor(private formBuilder: FormBuilder, private http: HttpClient) { 
-      this.form = this.formBuilder.group({
-          fullname: this.fullname,
-          email: this.email,
-          message: this.message,
-          honeypot: this.honeypot
-      })
-  }
+  constructor(private builder: FormBuilder, private message: MessageService) { }
 
   ngOnInit(): void {
+    this.FormData = this.builder.group({
+        name: new FormControl('', [Validators.required]),
+        email: new FormControl('', [Validators.required]),
+        message: new FormControl('', [Validators.required])
+    });  
   }
 
-//  onSubmit(value: any) {
-//      console.log(value);
-//  }
-
-  onSubmit() {
-    if (this.form.status == "VALID" && this.honeypot.value == "") {
-        this.form.disable(); // disable the form if it's valid to disable multiple submissions
-        var formData: any = new FormData();
-        formData.append("fullname", this.form.get("fullname")!.value);
-        formData.append("email", this.form.get("email")!.value);
-        formData.append("message", this.form.get("message")!.value);
-        this.isLoading = true; // sending the post request async so it's in progress
-        this.submitted = false; // hide the response message on multiple submits
-        this.http.post("YOUR GOOGLE WEB APP URL HERE", formData).subscribe(
-          (response: any) => {
-            // choose the response message
-            if (response["result"] == "success") {
-              this.responseMessage = "Thanks for the message! I'll get back to you soon!";
-            } else {
-              this.responseMessage = "Oops! Something went wrong... Reload the page and try again.";
-            }
-            this.form.enable(); // re enable the form after a success
-            this.submitted = true; // show the response message
-            this.isLoading = false; // re enable the submit button
-            console.log(response);
-          },
-          (error) => {
-            this.responseMessage = "Oops! An error occurred... Reload the page and try again.";
-            this.form.enable(); // re enable the form after a success
-            this.submitted = true; // show the response message
-            this.isLoading = false; // re enable the submit button
-            console.log(error);
-          }
-        );
-      } 
+  onSubmit(FormData: any) {
+    this.message.sendMessage(FormData).subscribe(res => {
+        document.getElementById("messageAlertSuccess")!.style.display = "block";
+        setTimeout(function () {
+            document.getElementById("messageAlertSuccess")!.style.display='none';
+        }, 5000);
+    }, error => {
+        document.getElementById("messageAlertFailure")!.style.display = "block";
+        setTimeout(function () {
+            document.getElementById("messageAlertFailure")!.style.display='none';
+        }, 5000);
+    });
+    console.log('hit');
+    console.log(FormData);
   }
 
 }
