c87c43f54c55e4f6607daf28e545f8ed2bb0f6b1
bbourassa <bnboura@cs.unc.edu>
2023-02-03 10:57:34 -0500

    mailing

--- src/app/send-email.php
+++ src/app/send-email.php
@@ -1,13 +1,38 @@
 <?php
 
-if(isset($_POST['submit'])) {
-    $to = 'bnbourassa@gmail.com';
-    $subject='Customer Inquiry';
-    $txt = $POST['message'];
-    $email = $_POST['email'];
-    $sender=$_POST['name'];
-}
+$to = 'bnbourassa@gmail.com';
+$subject='Customer Inquiry';
+$txt = $POST['message'];
+$email = $_POST['email'];
+$sender=$_POST['name'];
 
-mail($to, $subject, $txt, "From: $email");
+// mail($to, $subject, $txt, "From: $email");
+
+require "../../vendor/autoload.php";
+
+use PHPMailer\PHPMailer\PHPMailer;
+use PHPMailer\PHPMailer\SMTP;
+
+$mail = new PHPMailer(true);
+
+$mail->isSMTP();
+$mail->SMTPAuth = true;
+
+$mail->Host = "relay.sendinblue.com";
+$mail->SMTPSecure = PHPMailer::ENCRYPTION_STARTTLS;
+$mail->Port=587;
+
+$mail->Username = "bnbourassa@gmail.com";
+$mail->Password = "Iconic402!";
+
+$mail->setFrom($email, $sender);
+$mail->addAddress("bnbourassa@gmail.com", "Britney");
+
+$mail->Subject = $subject;
+$mail->Body = $txt;
+
+$mail->send();
+
+echo "email sent";
 
 ?>
